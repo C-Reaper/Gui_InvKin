340b04e6afca1998e5ead10b031962004ef42533
codeleaded <gnu.c.reaper@gmail.com>
2026-02-09 16:28:29 +0100

    init commit

---
+++ src/Main.c
@@ -0,0 +1,26 @@
+#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/InvKin.h"
+
+
+
+InvKin ik;
+
+void Setup(AlxWindow* w){
+    ik = InvKin_New();
+}
+void Update(AlxWindow* w){
+	InvKin_Update(&ik);
+
+	Clear(BLACK);
+
+	InvKin_Render(&ik,WINDOW_STD_ARGS);
+}
+void Delete(AlxWindow* w){
+	InvKin_Free(&ik);
+}
+
+int main(int argc,char** argv){
+    if(Create("IDE - 4.0",1900,1000,1,1,Setup,Update,Delete))
+        Start();
+    return 0;
+}
